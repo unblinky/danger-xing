59e46b90cabe3a6ca98740d0f051a260834d32b0
unblinky <>
2026-04-15 16:08:32 -0700

    Added crushed() logic.

--- Code/Chicken/Chicken.gd
+++ Code/Chicken/Chicken.gd
@@ -2,19 +2,28 @@ extends Area3D
 class_name Chicken
 
 @onready var graphics: Node3D = $Graphics
+@onready var lives_ui: Label = $UI/LivesUI
+
+var lives: int = 8
 
 
 func _ready() -> void:
 	area_entered.connect(on_collision)
+	lives_ui.text = "Lives: " + str(lives)
 
 
 func on_collision(area: Area3D):
 	if area is Goal:
 		print("Goal!!!!")
-	
-	# TODO: Add more collision types.
-	#if area is Bus:
-		#print("Goal!!!!")
+	if area is Car:
+		cruched()
+
+func cruched():
+	position = Vector3.ZERO
+	graphics.rotation_degrees.y = 0
+	print("Crushed!")
+	lives -= 1
+	lives_ui.text = "Lives: " + str(lives)
 
 
 func _process(delta: float) -> void:
