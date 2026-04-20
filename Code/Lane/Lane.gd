83c6f9fe2c57ca9f63951f7a588b737eefbe51c8
unblinky <>
2026-04-20 14:50:45 -0700

    Expose the speed limit. Setup more lanes.

--- Code/Lane/Lane.gd
+++ Code/Lane/Lane.gd
@@ -1,6 +1,7 @@
 extends MeshInstance3D
 class_name Lane
 
-var speed_limit = 2.0 # meters per second.
+@export var speed_limit = 2.0 # meters per second.
+
 var entrance_point = -20 # X units
 var exit_point = 20
